cb85692608165f7f757de91f1a20f0c4c71732c7
MohsinNajmuddin <mohsin.njm@hotmail.com>
2021-10-17 21:25:54 +0500

    added user image code

--- src/service/post.service.js
+++ src/service/post.service.js
@@ -2,7 +2,7 @@ const PostModel = require("../model/post.model");
 const Post = PostModel.Post;
 const BookmarkPostModel = require("../model/bookmark.model");
 const BookmarkPost = BookmarkPostModel.BookmarkPost;
-
+const userService = require("./user.service");
 const activityService = require("./activity.service");
 const notificationService = require("./notification.service");
 const { getLinkPreview } = require("link-preview-js");
@@ -73,6 +73,17 @@ exports.getAllPostsByTags = async (tags) => {
 };
 
 exports.createPost = async (author, url, tags) => {
+
+  const user = await userService.getUserByUsername(author);
+  if (!user) {
+    user = {};
+  }
+
+  const userMeta = await userService.getUserMeta(author);
+  if (!userMeta) {
+    userMeta = {};
+  }
+
   const metadata = await extractUrlMetadata(url);
 
   const post = await Post.create({
@@ -81,7 +92,7 @@ exports.createPost = async (author, url, tags) => {
     title: metadata.title,
     description: metadata.description,
     image: metadata.images[0],
-    author_image: metadata.images[0],
+    author_image: userMeta.image || user.google_picture || '',
     site_name: metadata.siteName,
     favicon: metadata.favicons[0],
     created_at: new Date(),
